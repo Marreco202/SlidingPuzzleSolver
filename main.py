7803d4687d64dc063f7abd8af2ea238078bd7752
João Victor Godinho W. <slygodinho@gmail.com>
2024-05-16 14:28:53 -0300

    commit teste

--- main.py
+++ main.py
@@ -3,5 +3,12 @@ Trabalho 2 - Análise de Algoritmos
 
 Alunos:
     Guilherme Ponce - <inserir matrícula>
-
+    João Victor Godinho - 2011401
 '''
+
+
+
+def teste():
+    print("hello world!")
+    return
+
