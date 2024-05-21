3f39e9a867d2eb2aba684fcd9ac4778829b67897
João Victor Godinho W. <slygodinho@gmail.com>
2024-05-21 18:46:36 -0300

    better but still broken

--- main.py
+++ main.py
@@ -24,12 +24,14 @@ total_combo_list = []
 def permutation(ini,n,combo):
     if(ini == n):
         for i in range(0,n):
-            combo.insert(i+1,'x')
+            combo.insert(i,'x')
             total_combo_list.append(combo)
+            print(combo,i)
+            combo.remove('x')
         return
 
     for i in range(0,ini):
-        combo.insert(i+1,ini)
+        combo.insert(i,ini)
         permutation(ini+1,n,combo)
 
 
@@ -38,5 +40,5 @@ permutation(1,8,list([]))
 # print(total_combo_list)
 print("tamanho da lista: ",len(total_combo_list))
 
-f = open("out.txt","w")
-f.write(str(total_combo_list))
+# f = open("out.txt","w")
+# f.write(str(total_combo_list))
